3238ff16d97b2850ef0c7ad8c35f5267adfae10d
Right Fix <righteousnessude@gmail.com>
2025-07-05 20:00:16 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from kivy.app import MDApp
-from kivy.uix.screenmanager import MDScreen
+import time
+from kivy.app import App
+from pybit.exceptions import FailedRequestError
+from kivy.uix.screenmanager import Screen
+from assets.backend import code
 
 class Screen1(Screen):
   pass
@@ -11,9 +14,19 @@ class MainApp(App):
     key  = ["","",""]
     
     def build(self):
-      
       #self.theme_cls.primary_palette = "Blue"
-      #self.theme_cls.theme_style = "Light"
+      #self.theme_cls.theme_style = "Light
+      
       return super().build()
+    def switch(instance,value):
+        try:
+          orders = code.trading(value[0],value[1],value[2])
+          change_to_string = "?".join(orders)
+          return change_to_string.replace("?","\n\n")
+          
+        except FailedRequestError:
+           orders  = "Turn On VPN Or No Internet Connection"
+           return orders
     
+      
 MainApp().run()
